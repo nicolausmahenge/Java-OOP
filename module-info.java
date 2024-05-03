6631af60535c3b5ce7ba8193bafa61900c3a5ef0
nicolausmahenge <151054381+nicolausmahenge@users.noreply.github.com>
2024-05-03 14:49:56 +0300

    Update module-info.java

--- module-info.java
+++ module-info.java
@@ -1,9 +1,28 @@
-/**
- * 
- */
-/**
- * @author nicolaus
- *
- */
-module learning.java {
+class Animal{
+	public void animalSound() {
+		System.out.println("The animal makes sound");
+	}
+}
+
+class Pig extends Animal {
+	public void animalSound() {
+		System.out.println("The pig says: wee wee");
+	}
+
+class Dog extends Animal {
+	public void animalSound() {
+		System.out.println("The dog says: bow bow");
+	}
+}
+
+class Main {
+	public static void main(String[] args) {
+		Animal myAnimal = new Animal();
+		Animal myPig = new Pig();
+		Animal myDog = new Dog();
+		
+		myAnimal.animalSound();
+		myPig.animalSound();
+		myDog.animalSound();
+	}
 }
